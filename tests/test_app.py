2a52da3d1c92571b246fd61eef8c3e4ec48e9c3e
Anitha <122508755+Anitha-110@users.noreply.github.com>
2026-09-01 16:24:58 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,29 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(
+    0,
+    str(Path(__file__).resolve().parents[1] / "backend")
+)
+
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
